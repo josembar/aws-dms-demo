7588280e7be3dfc2d4a818e361e8842cad45e3c3
josembar <jose.barrantes@hotmail.es>
2026-07-01 10:48:06 -0500

    created data

---
+++ mysql-source/init/02-data.sql
@@ -0,0 +1,9 @@
+USE company;
+
+INSERT INTO employee(first_name,last_name,department,salary)
+VALUES
+('John','Doe','Engineering',75000),
+('Alice','Smith','Finance',64000),
+('Bob','Johnson','Sales',58000),
+('Maria','Lopez','HR',61000),
+('David','Brown','Engineering',81000);
